abd62259251036450714c6fbde7773e5b71f7a2f
Ivan Shiyan <ishiyan@binck.nl>
2020-02-15 16:37:35 +0100

    updated ng to 9 and .net to 3.1

--- IdentityServer/Extensions/X509Extensions.cs
+++ IdentityServer/Extensions/X509Extensions.cs
@@ -6,13 +6,11 @@ namespace IdentityServer.Extensions
     {
         public static X509Certificate2 GetX509Certificate(this string thumbprint)
         {
-            using (X509Store certStore = new X509Store(StoreName.My, StoreLocation.CurrentUser))
-            {
-                certStore.Open(OpenFlags.ReadOnly);
-                X509Certificate2Collection certCollection = certStore.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, false);
-                certStore.Close();
-                return certCollection.Count > 0 ? certCollection[0] : null;
-            }
+            using X509Store certStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            certStore.Open(OpenFlags.ReadOnly);
+            X509Certificate2Collection certCollection = certStore.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, false);
+            certStore.Close();
+            return certCollection.Count > 0 ? certCollection[0] : null;
         }
     }
 }
--- IdentityServerHostNg/ClientApp/src/app/app.module.ts
+++ IdentityServerHostNg/ClientApp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import 'hammerjs';
+
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
